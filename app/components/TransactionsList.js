6fe63e56a26973d70e080bf6b3c5be07d58524f2
Elisabeth Gross <elisabethgross568@gmail.com>
2016-10-24 14:26:23 -0400

    put fetchDaTransactions into componentDidMount

--- app/components/TransactionsList.js
+++ app/components/TransactionsList.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default ({ transactions, fetchDaTransactions}) => (
-  <div>
-    <button onClick={fetchDaTransactions}>Get the transactions</button>
-    <ul>
-      {transactions && transactions.map(transaction => (
-        <li><span>name: {transaction.name} </span><span>amount: {transaction.amount}</span></li>
-      ))}
-    </ul>
-  </div>
-);
+export default class TransactionList extends Component {
+
+  componentDidMount() {
+    this.props.fetchDaTransactions();
+  }
+
+  render() {
+    const { transactions } = this.props;
+    return (
+      <div>
+        <ul>
+          {transactions && transactions.map(transaction => (
+            <li><span>name: {transaction.name}, </span><span>amount: ${transaction.amount}</span></li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
--- public/bundle.js
+++ public/bundle.js
@@ -22470,48 +22470,73 @@
 	  value: true
 	});
 	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(1);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	exports.default = function (_ref) {
-	  var transactions = _ref.transactions;
-	  var fetchDaTransactions = _ref.fetchDaTransactions;
-	  return _react2.default.createElement(
-	    'div',
-	    null,
-	    _react2.default.createElement(
-	      'button',
-	      { onClick: fetchDaTransactions },
-	      'Get the transactions'
-	    ),
-	    _react2.default.createElement(
-	      'ul',
-	      null,
-	      transactions && transactions.map(function (transaction) {
-	        return _react2.default.createElement(
-	          'li',
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var TransactionList = function (_Component) {
+	  _inherits(TransactionList, _Component);
+	
+	  function TransactionList() {
+	    _classCallCheck(this, TransactionList);
+	
+	    return _possibleConstructorReturn(this, (TransactionList.__proto__ || Object.getPrototypeOf(TransactionList)).apply(this, arguments));
+	  }
+	
+	  _createClass(TransactionList, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      this.props.fetchDaTransactions();
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var transactions = this.props.transactions;
+	
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'ul',
 	          null,
-	          _react2.default.createElement(
-	            'span',
-	            null,
-	            'name: ',
-	            transaction.name,
-	            ' '
-	          ),
-	          _react2.default.createElement(
-	            'span',
-	            null,
-	            'amount: ',
-	            transaction.amount
-	          )
-	        );
-	      })
-	    )
-	  );
-	};
+	          transactions && transactions.map(function (transaction) {
+	            return _react2.default.createElement(
+	              'li',
+	              null,
+	              _react2.default.createElement(
+	                'span',
+	                null,
+	                'name: ',
+	                transaction.name,
+	                ', '
+	              ),
+	              _react2.default.createElement(
+	                'span',
+	                null,
+	                'amount: $',
+	                transaction.amount
+	              )
+	            );
+	          })
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return TransactionList;
+	}(_react.Component);
+	
+	exports.default = TransactionList;
 
 /***/ }
 /******/ ]);
