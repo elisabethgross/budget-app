f4b069bad6787fb1b028a765f61a73852771aeb9
Elisabeth Gross <elisabethgross568@gmail.com>
2016-10-24 11:35:14 -0400

    made routes work

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 module.exports = router;
+
+router.use('/transactions', require('./transactions'));
+
+
---
+++ routes/api/transactions.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const Transactions = require('../../db/models').Transactions;
+module.exports = router;
+
+
+router.get('/', function (req, res, next) {
+  Transactions.findAll()
+  .then(transactions => res.json(transactions))
+  .catch(next);
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  Transactions.create(req.body)
+  .then(createdTransaction => res.json(createdTransaction))
+  .catch(next);
+});
+
